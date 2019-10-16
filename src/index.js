e0934060da5e37c01b868cf701055d9def1b8e87
zan007 <pploszaj91@gmail.com>
2019-10-16 22:50:30 +0200

    save songs from one specific channel to database

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ const url = 'https://open.fm';
 scrapp = async () => {
     const browser = await puppeteer.launch({ slowMo: 300 });
     const page = await browser.newPage();
+
     await page.setViewport({ width: 1920, height: 926 });
     await page.goto(url);
 
@@ -33,26 +34,45 @@ scrapp = async () => {
 
     async function addChannelSongs (data, channelData) {
         const channel = data.channels.find((item) => item.id === channelData.id);
+        const channelName = databaseConnector.escape(channelData.name);
 
-        const track = channel.tracks[0];
-        console.log('channel', channel);
-        //channel.tracks.forEach((track) => {
+        channel.tracks.forEach((track) => {
             const {song} = track;
-            console.log('song', song);
+            const artistName = databaseConnector.escape(song.artist);
+            const songName = databaseConnector.escape(song.title);
+            const albumName = databaseConnector.escape(song.album.title);
+            const albumYear = databaseConnector.escape(song.album.year).toString();
+
+            console.log('song', artistName,
+            songName,
+            albumName,
+            albumYear);
 
             try {
                 databaseConnector.transaction(`
-                    INSERT INTO station (name) VALUES ('${channelData.name}');
-                    INSERT INTO artist (name) VALUES ('${song.artist}');
+                    INSERT INTO station (name) SELECT * FROM (SELECT ${channelName}) as tmp where not exists (select name from station where name=${channelName}) limit 1;
+                    INSERT INTO artist (name) SELECT * FROM (SELECT ${artistName}) as tmp where not exists (select name from artist where name = ${artistName}) limit 1;
+                    INSERT INTO album (album_artist, name, year) SELECT * FROM (SELECT (SELECT id_artist FROM artist WHERE name = ${artistName}), ${albumName}, ${albumYear}) as tmp where not exists (select name from album where name = ${albumName}) limit 1;
+                    INSERT INTO song (title, song_album) VALUES (${songName}, (SELECT id_album from album where name = ${albumName} limit 1));
+                    INSERT INTO song_station (id_song, id_station) VALUES ((SELECT id_song from song where title = ${songName} limit 1), (SELECT id_station FROM STATION WHERE name = ${channelName} limit 1));
+                `);
+            } catch (err) {
+                console.error('catch transaction', err);
+                console.error('song', artistName,
+                    songName,
+                    albumName,
+                    albumYear);
+
+            }
+        })
+    }
+
+    /*INSERT INTO station (name) VALUES ('${channelData.name}') ON DUPLICATE KEY UPDATE name = name;
+                    INSERT INTO artist (name) VALUES ('${song.artist}') ON DUPLICATE KEY UPDATE name = name;
                     INSERT INTO album (album_artist, name, year) values ((SELECT id_artist FROM artist WHERE name = '${song.artist}'), '${song.album.title}', '${song.album.year}');
                     INSERT INTO song (title, song_album) VALUES ('${song.title}', (SELECT id_album from album where name = '${song.album.title}'));
                     INSERT INTO song_station (id_song, id_station) VALUES ((SELECT id_song from song where title = '${song.title}'), (SELECT id_station FROM STATION WHERE name = '${channelData.name}'));
-                `);
-            } catch (err) {
-                console.log('catch transaction', err);
-            }
-        //})
-    }
+                    */
 
     /*async function updateChannels (channels) {
         channels.map((channel) => {
--- src/services/database-connector.js
+++ src/services/database-connector.js
@@ -15,7 +15,7 @@ class DatabaseConnector {
         return new Promise ((resolve, reject) => {
             return this.connection.connect((err) => {
                 if (err) {
-                    reject(err);
+                    return reject(err);
                 }
 
                 return resolve();
@@ -27,7 +27,7 @@ class DatabaseConnector {
         return new Promise ((resolve, reject) => {
             this.connection.end((err) => {
                 if (err) {
-                    reject(err);
+                    return reject(err);
                 }
 
                 return resolve();
@@ -39,7 +39,7 @@ class DatabaseConnector {
         return new Promise((resolve, reject) => {
             this.connection.query(query, (err, result, fields) => {
                 if (err) {
-                    reject(err);
+                    return reject(err);
                 }
 
                 console.log('query result', result);
@@ -49,12 +49,16 @@ class DatabaseConnector {
         })
     }
 
+    escape (data) {
+        return this.connection.escape(data);
+    }
+
     transaction (query) {
         return new Promise((resolve, reject) => {
             this.connection.beginTransaction((err) => {
                 if (err) {
                     return this.connection.rollback(() => {
-                        reject();
+                        return reject(err);
                     });
                 }
 
@@ -62,13 +66,13 @@ class DatabaseConnector {
                     return this.connection.commit((err) => {
                         if (err) {
                             return this.connection.rollback(() => {
-                                reject();
+                                return reject(err);
                             });
                         }
                     })
                 }, (err) => {
                     return this.connection.rollback(() => {
-                        reject();
+                        return reject(err);
                     });
                 })
             })
@@ -81,11 +85,20 @@ module.exports = DatabaseConnector;
 /*
 START TRANSACTION;
 
-INSERT INTO station (name) VALUES ('Rocks');
-INSERT INTO artist (name) VALUES ('Linkin Park');
+INSERT INTO station (name) VALUES ('Rocks') ON DUPLICATE KEY UPDATE name = name;
+INSERT INTO artist (name) VALUES ('Linkin Park') ON DUPLICATE KEY UPDATE name = name;
 INSERT INTO album (album_artist, name, year) values ((SELECT id_artist FROM artist WHERE name = 'Linkin Park'), 'Meteora', '1997');
 INSERT INTO song (title, song_album) VALUES ('Numb', (SELECT id_album from album where name = 'Meteora'));
 INSERT INTO song_station (id_song, id_station) VALUES ((SELECT id_song from song where title = 'Numb'), (SELECT id_station FROM STATION WHERE name = 'Rocks'));
 
 COMMIT;
  */
+
+/*
+INSERT INTO station (name) SELECT * FROM (SELECT 'Rocks') as tmp where not exists (select name from station where name='Rocks') limit 1;
+INSERT INTO artist (name) SELECT * FROM (SELECT 'Linkin Park') as tmp where not exists (select name from artist where name = 'Linkin Park') limit 1;
+
+INSERT INTO album (album_artist, name, year) SELECT * FROM (SELECT (SELECT id_artist FROM artist WHERE name = 'Linkin Park'), 'Meteora', '1997') as tmp where not exists (select name from album where name = 'Meteora') limit 1;
+INSERT INTO song (title, song_album) VALUES ('Numb', (SELECT id_album from album where name = 'Meteora' limit 1));
+INSERT INTO song_station (id_song, id_station) VALUES ((SELECT id_song from song where title = 'Numb' limit 1), (SELECT id_station FROM STATION WHERE name = 'Rocks' limit 1));
+* */
